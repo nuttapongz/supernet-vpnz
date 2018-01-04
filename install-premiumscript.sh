38c337a2034ede1f80712064e7abf77888aa2865
nuttapongz <33756748+nuttapongz@users.noreply.github.com>
2018-01-04 08:13:23 +0700

    Update install-premiumscript.sh

--- install-premiumscript.sh
+++ install-premiumscript.sh
@@ -19,11 +19,39 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.tar.gz "https://raw.githubusercontent.com/nuttapongz/supernet-vpnz/master/premium-script.tar.gz"
-tar -xvf premium-script.tar.gz
-rm -f premium-script.tar.gz
-
+wget -O premium-script "https://raw.githubusercontent.com/nuttapongz/premium-script/master/premium-script"
+wget -O alluser-pptp "https://raw.githubusercontent.com/nuttapongz/premium-script/master/alluser-pptp"
+wget -O edit-port "https://raw.githubusercontent.com/nuttapongz/premium-script/master/edit-port"
+wget -O edit-port-openssh "https://raw.githubusercontent.com/nuttapongz/premium-script/master/edit-port-openssh"
+wget -O edit-port-openvpn "https://raw.githubusercontent.com/nuttapongz/premium-script/master/edit-port-openvpn"
+wget -O edit-port-squid "https://raw.githubusercontent.com/nuttapongz/premium-script/master/edit-port-squid"
+wget -O limit.sh "https://raw.githubusercontent.com/nuttapongz/premium-script/master/limit.sh"
+wget -O listpassword "https://raw.githubusercontent.com/nuttapongz/premium-script/master/listpassword"
+wget -O log-ban "https://raw.githubusercontent.com/nuttapongz/premium-script/master/log-ban"
+wget -O log-limit "https://raw.githubusercontent.com/nuttapongz/premium-script/master/log-limit"
+wget -O trial "https://raw.githubusercontent.com/nuttapongz/premium-script/master/trial"
+wget -O user-add "https://raw.githubusercontent.com/nuttapongz/premium-script/master/user-add"
+wget -O user-delete "https://raw.githubusercontent.com/nuttapongz/premium-script/master/user-delete"
+wget -O user-delete-expired "https://raw.githubusercontent.com/nuttapongz/premium-script/master/user-delete-expired"
+wget -O user-password "https://raw.githubusercontent.com/nuttapongz/premium-script/master/user-passwordฅ
 cp /usr/local/bin/premium-script /usr/local/bin/menu
+
+chmod +x /usr/local/bin/premium-script
+chmod +x /usr/local/bin/alluser-pptp
+chmod +x /usr/local/bin/edit-port
+chmod +x /usr/local/bin/edit-port-openssh
+chmod +x /usr/local/bin/edit-port-openvpn
+chmod +x /usr/local/bin/edit-port-squid
+chmod +x /usr/local/bin/limit.sh
+chmod +x /usr/local/bin/listpassword
+chmod +x /usr/local/bin/log-ban
+chmod +x /usr/local/bin/log-limit
+chmod +x /usr/local/bin/trial
+chmod +x /usr/local/bin/user-add
+chmod +x /usr/local/bin/user-delete
+chmod +x /usr/local/bin/user-delete-expired
+chmod +x /usr/local/bin/user-password
+
 
 chmod +x /usr/local/bin/trial
 chmod +x /usr/local/bin/user-add
